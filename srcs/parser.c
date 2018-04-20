16581e2ec7380855f53b69caffaf33c595d1c4c9
root <root@debianjq.jquenel>
2018-04-20 15:56:07 +0200

    parsing nearly complete

--- srcs/parser.c
+++ srcs/parser.c
@@ -4,13 +4,11 @@ static void	init_player(t_bushi *player)
 {
 	int	i;
 
-	i = MAX_PLAYERS;
+	i = MAX_PLAYERS + 1;
 	while (i--)
 	{
 		player[i].pnum = 0;
-		player[i].carry = 0;
 		player[i].live = 0;
-		ft_bzero(player[i].reg, REG_NUMBER + 1);
 		ft_bzero(player[i].name, PROG_NAME_LENGTH + 1);
 	}
 }
@@ -22,7 +20,6 @@ static void	init_state(t_sumego *state)
 	state->c_todie = CYCLE_TO_DIE;
 	state->c_delta = CYCLE_DELTA;
 	state->l_count = 0;
-	state->l_count = 0;
 	state->l_checks = 0;
 	state->l_limit = MAX_CHECKS;
 	state->dump_limit = -1;
@@ -31,6 +28,9 @@ static void	init_state(t_sumego *state)
 static int	init_core(t_sen *core)
 {
 	core->arena.size = MEM_SIZE;
+	core->pcount = 0;
+	core->proc = NULL;
+	core->opt = 0;
 	ft_bzero(&core->arena.field, MEM_SIZE);
 	init_state(&core->state);
 	init_player(&core->player);
@@ -44,19 +44,19 @@ static int	check_valid_define(void)
 int		parser(int argc, char **argv, t_sen *core)
 {
 	int	i;
-	int	pc;
 
 	if (!check_valid_define())
 		return (1);
 	init_core(core);
 	get_options(&argc, &argv, core);
-	if ((pc = count_players(argc, argv)) < 2)
+	if ((core->pcount = count_players(argc, argv)) < 2)
 		return (1);
-	while (i < pc)
+	i = 0;
+	while (i < core->pcount)
 	{
-		if (create_player(&argc, &argv, core, pc) == -1)
+		if (create_player(&argc, &argv, core, i) == -1)
 			return (1);
-		i--;
+		i++;
 	}
 	return (0);
 }
