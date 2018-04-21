9638708bcbf233593572c26e91e51e88b2521dfb
Sylvain BOILARD <sboilard@student.42.fr>
2018-04-22 00:44:08 +0200

    Minor modifications.

--- asm/parser.ml
+++ asm/parser.ml
@@ -7,8 +7,8 @@ module List =
       | hd :: tl when mem hd l -> union l tl
       | hd :: tl -> union (hd :: l) tl
 
-    let replace_assoc key new_value list =
-      (key, new_value) :: remove_assoc key list
+    let replace_assoc key value list =
+      (key, value) :: remove_assoc key list
   end
 
 type non_terminal =
@@ -22,6 +22,18 @@ type non_terminal =
   | `OperandNext
   | `Operand
   | `Val ]
+
+let all_non_terminals : non_terminal list =
+  [ `S
+  ; `ExpectComment
+  ; `ExpectName
+  ; `Comment
+  ; `Name
+  ; `Label
+  ; `Instruction
+  ; `OperandNext
+  ; `Operand
+  ; `Val ]
 
 type terminal =
   [ `LineSeparator
@@ -41,30 +53,6 @@ type symbol = [ non_terminal | terminal ]
 type symbol_narrow =
   | NonTerminal of non_terminal
   | Terminal of terminal
-
-let rules : (non_terminal * symbol list) list = [
-    `S, [`LineSeparator; `S];
-    `S, [`Name; `LineSeparator; `ExpectComment];
-    `S, [`Comment; `LineSeparator; `ExpectName];
-    `ExpectComment, [`LineSeparator; `ExpectComment];
-    `ExpectComment, [`Comment; `LineSeparator; `Label];
-    `ExpectName, [`LineSeparator; `ExpectName];
-    `ExpectName, [`Name; `LineSeparator; `Label];
-    `Comment, [`CommentCommand; `StringLiteral];
-    `Name, [`NameCommand; `StringLiteral];
-    `Label, [`LabelString; `Instruction];
-    `Label, [`Instruction];
-    `Label, [];
-    `Instruction, [`Operator; `Operand; `OperandNext; `LineSeparator; `Label];
-    `Instruction, [`LineSeparator; `Label];
-    `OperandNext, [`SeparatorChar; `Operand; `OperandNext];
-    `OperandNext, [];
-    `Operand, [`DirectChar; `Val];
-    `Operand, [`Val];
-    `Operand, [`Register];
-    `Val, [`LabelVal];
-    `Val, [`Number]
-  ]
 
 let narrow_symbol_type : symbol -> _ = function
   | `S
@@ -91,6 +79,30 @@ let narrow_symbol_type : symbol -> _ = function
   | `Number as s
     -> Terminal s
 
+let rules : (non_terminal * symbol list) list = [
+    `S, [`LineSeparator; `S];
+    `S, [`Name; `LineSeparator; `ExpectComment];
+    `S, [`Comment; `LineSeparator; `ExpectName];
+    `ExpectComment, [`LineSeparator; `ExpectComment];
+    `ExpectComment, [`Comment; `LineSeparator; `Label];
+    `ExpectName, [`LineSeparator; `ExpectName];
+    `ExpectName, [`Name; `LineSeparator; `Label];
+    `Comment, [`CommentCommand; `StringLiteral];
+    `Name, [`NameCommand; `StringLiteral];
+    `Label, [`LabelString; `Instruction];
+    `Label, [`Instruction];
+    `Label, [];
+    `Instruction, [`Operator; `Operand; `OperandNext; `LineSeparator; `Label];
+    `Instruction, [`LineSeparator; `Label];
+    `OperandNext, [`SeparatorChar; `Operand; `OperandNext];
+    `OperandNext, [];
+    `Operand, [`DirectChar; `Val];
+    `Operand, [`Val];
+    `Operand, [`Register];
+    `Val, [`LabelVal];
+    `Val, [`Number]
+  ]
+
 let compute_first_sets rules =
   let open List in
   let rec aux first_sets = function
@@ -110,11 +122,9 @@ let compute_first_sets rules =
           else aux (replace_assoc nt (hd :: first) first_sets) tl
        | NonTerminal hd when mem `Epsilon (assoc hd first_sets) ->
           let new_first =
-            union
-              first
-              (union
-                 (filter ((<>) `Epsilon) (assoc hd first_sets))
-                 (assoc nt (aux first_sets [nt, wtl])))
+            filter ((<>) `Epsilon) (assoc hd first_sets)
+            |> union (assoc nt (aux first_sets [nt, wtl]))
+            |> union first
           in
           if new_first == first
           then aux first_sets tl
@@ -126,9 +136,9 @@ let compute_first_sets rules =
           else aux (replace_assoc nt new_first first_sets) tl
   in
   let rec loop acc =
-    let acc_next = aux acc rules in
-    if acc != acc_next then loop acc_next else acc
+    let new_acc = aux acc rules in
+    if acc != new_acc then loop new_acc else acc
   in
-  split rules |> fst |> union [] |> rev_map (fun s -> s, []) |> loop
+  loop (rev_map (fun s -> s, []) all_non_terminals)
 
 let first_sets = compute_first_sets rules
